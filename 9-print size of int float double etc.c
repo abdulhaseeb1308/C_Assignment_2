01ed4849a54d638e849d766b2869aab3b79de411
Abdul Haseeb <127509119+abdulhaseeb1308@users.noreply.github.com>
2023-07-11 17:58:52 +0530

    Update 9-print size of int float double etc.c

--- 9-print size of int float double etc.c
+++ 9-print size of int float double etc.c
@@ -1,5 +1,10 @@
-#include<stdio.h>
-int main ()
-{
-	sizeof(int);
+#include <stdio.h>
+
+int main() {
+    printf("Size of int: %zu bytes\n", sizeof(int));
+    printf("Size of float: %zu bytes\n", sizeof(float));
+    printf("Size of char: %zu byte\n", sizeof(char));
+    printf("Size of double: %zu bytes\n", sizeof(double));
+
+    return 0;
 }
